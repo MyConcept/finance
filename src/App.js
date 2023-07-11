7dfda5b61ed7d0d9def517546e74247144dac8d0
MyConcept <leader@myconcept.pro>
2023-07-11 00:42:46 +0000

    committed10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
-import './App.css';
+import React from 'react';
+import './App.scss';
 import Navbar from "./Navbar/Navbar";
 import Layout from "./Layout/Layout"
+import Footer from './Footer/Footer';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Layout/>
+      <Footer/>
     </div>
   );
 }
